a76e60a5a7b8016acef9d00be8737de7e5a4bfa8
Sekar Wibowo <sekarsaraswatiw@gmail.com>
2024-09-15 10:55:40 +0700

    Update comments

--- employees.py
+++ employees.py
@@ -6,33 +6,39 @@ def main_menu():
 
         #Raise error if input is not an integer
         try:
-            option = int(option)  
+            option = int(option)
+             #Print error message if option is out of range
+            if option >= 7:
+                print('Option out of range! Please enter a number from the menu: ')
+                continue
+
+            #Display Menu
+            elif option == 1:
+                print('Display')
+            
+            #Summary Menu
+            elif option == 2:
+                print('Summary')
+
+            #Create New Employee Menu
+            elif option == 3:
+                print('Create New Employee')
+
+            #Update Employee Menu
+            elif option == 4:
+                print('Update Employee Data')
+
+            #Delete Employee Menu
+            elif option == 5:
+                print('Delete Employee Data')
+
+            #End Program
+            else:
+                break  
+
         except:
             print('Wrong input! Please enter a number') 
 
-        #Print error message if option is out of range
-        if option >= 7:
-            print('Option out of range! Please enter a number from the menu: ')
-            continue
-        
-        #Display Menu
-        elif option == 1:
-            print('Display')
-        
-        #Summary Menu
-        elif option == 2:
-            print('Summary')
+       
 
-        #Create New Employee Menu
-        elif option == 3:
-            print('Create New Employee')
-
-        elif option == 4:
-            print('Update Employee Data')
-
-        elif option == 5:
-            print('Delete Employee Data')
-
-        else:
-            break
-        
+main_menu()
