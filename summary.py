55e82ba61de2ef85d53ce8d090be068b8565ebbf
Sekar Wibowo <sekarsaraswatiw@gmail.com>
2024-09-21 20:02:42 +0700

    Summary Function Complete

--- summary.py
+++ summary.py
@@ -36,7 +36,7 @@ emp_dict = {
 summary_prompt = '''
 Display Employee Summary
 1. Display Overall Summary
-2. Filter Summary by Categories
+2. Display Summary by Department
 3. Back to Main Menu
 '''
 
@@ -51,6 +51,24 @@ def summary_menu(emp_dict=None):
     salary = [val[5] for val in employees.values()]
     exp = [val[6] for val in employees.values()]
 
+    def average(col):
+        return (sum(col)//len(col))
+
+    def filter_dept(dept=None):
+        emp_gender = []
+        emp_age = []
+        emp_salary = []
+        emp_exp = []
+        for k, v in employees.items():
+            if v[4] == dept:
+                emp_gender.append(v[1])
+                emp_age.append(v[2])
+                emp_salary.append(v[5])
+                emp_exp.append(v[6])
+
+        return emp_gender, emp_age, emp_salary, emp_exp
+
+
     while True:
         print(summary_prompt)
         opt = input('Enter an option: ')
@@ -63,24 +81,123 @@ def summary_menu(emp_dict=None):
         
         #Employee summary menu
         if opt == 1:
-
-            print('\nEmployee Summary')
-            print('------------------------------------------')
-            print(f'Total Employees: {len(id)}')
-            print(f'Male Employees: {gender.count('M')}')
-            print(f'Female Employees: {gender.count('F')}')
-            print(f'Average Age: {sum(age)//len(age)}')
+            print('\n====================================')
+            print(f'{'Employee Summary':^30}')
+            print('====================================')
+            print(f'Total Departments: {len(set(dept))}')
+            print(f'{', '.join(set(dept))}')
+            print()
+            print(f'Total Employees: {len(id):>10}')
+            print(f'Male Employees: {gender.count('M'):>11}')
+            print(f'Female Employees: {gender.count('F'):>10}')
+            print()
+            print(f'Average Age: {average(age)}')
             print(f'Oldest Employee Age: {max(age)}')
             print(f'Youngest Employee Age: {min(age)}')
-            print(f'Average Annual Salary: ${sum(salary)//len(salary):,}')
+            print()
+            print(f'Average Annual Salary: ${average(salary):,}')
             print(f'Highest Salary: ${max(salary):,}')
             print(f'Lowest Salary: ${min(salary):,}')
-            print(f'Average Years of Experience: {sum(exp)//len(exp)}')
+            print()
+            print(f'Average Years of Experience: {average(exp)}')
 
-        #Filter summary 
+        #--------------------------------------------------------------------------------------------------------
+        #Display Summary by Department Menu
         elif opt == 2:
-            print('Filter summary')
+            #Define department names
+            columns = ['Finance', 'Marketing', 'IT', 'Production', 'Sales', 'HR', 'Back to display menu']
+
+            #Display menu to input filter category
+            print('Filter Employee Data by ')
+            for col in list(enumerate(columns, 1)):
+                print(col[0], col[1])
+            print()
+
+            #Choose filter
+            #Error if input is a string
+            try:
+                opt = int(input('Choose a department: '))
+            except:
+                print('Wrong value! Must be a number')
+                continue
             
+            #If number input is not from the menu
+            if opt > 7 or opt == 7:
+                print('Choose the number from the menu!')
+                continue
+
+            #Go back to main display menu
+            if opt == 7:
+                break
+
+            # emp_gender = []
+            # emp_age = []
+            # emp_salary = []
+            # emp_exp = []
+
+            #Summary of Finance Department 
+            if opt == 1:
+                print()
+                print('================================')
+                print('Summary of Finance Department')
+                print('================================')
+                
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('Finance')
+
+            elif opt == 2:
+                print()
+                print('================================')
+                print('Summary of Marketing Department')
+                print('================================')
+
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('Marketing')
+
+            elif opt == 3:
+                print()
+                print('================================')
+                print('Summary of IT Department')
+                print('================================')
+
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('IT')
+
+            elif opt == 4:
+                print()
+                print('================================')
+                print('Summary of Production Department')
+                print('================================')
+
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('Production')
+
+            elif opt == 5:
+                print()
+                print('================================')
+                print('Summary of Sales Department')
+                print('================================')
+
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('Sales')
+            
+            elif opt == 6:
+                print()
+                print('==== ============================')
+                print('Summary of HR Department')
+                print('================================')
+
+                emp_gender, emp_age, emp_salary, emp_exp = filter_dept('HR')
+
+
+            print(f'Total Employees: {len(emp_gender)}')
+            print(f'Male Employees: {emp_gender.count('M'):>11}')
+            print(f'Female Employees: {emp_gender.count('F'):>10}')
+            print()
+            print(f'Average Age: {average(emp_age)}')
+            print(f'Oldest Employee Age: {max(emp_age)}')
+            print(f'Youngest Employee Age: {min(emp_age)}')
+            print()
+            print(f'Average Annual Salary: ${average(emp_salary):,}')
+            print(f'Highest Salary: ${max(emp_salary):,}')
+            print(f'Lowest Salary: ${min(emp_salary):,}')
+            print()
+            print(f'Average Years of Experience: {average(emp_exp)}')
 
         #Go back to main menu
         elif opt == 3:
